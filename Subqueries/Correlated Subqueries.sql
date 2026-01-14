205dec3d189ddd4155c7e990a4eed1b321a6d44a
Christian Chesire <christianck.chesire@gmail.com>
2026-01-14 11:29:36 +0300

    Implemented correlated subquries, with sections on running previous or next values and using running aggregates.

--- Subqueries/Correlated Subqueries.sql
+++ Subqueries/Correlated Subqueries.sql
@@ -87,3 +87,73 @@ WHERE country = N'Spain'
     (SELECT * FROM Sales.Orders AS O
 	 WHERE O.custid = C.custid);
 
+----------------------------------------------------------------------
+-- Returning Previous Or Next Values
+----------------------------------------------------------------------
+-- Suppose you need to query the Orders table in the TSQLV6 database
+-- and return, for each order, information about the current orders
+-- and also the previous order ID. The tricky part is that the concept
+-- of "previous" implies order, and rows in a table have no order.
+-- One way to achieve this objective is with a T-SQL expression that
+-- means "the maximum value that is smaller than the current value."
+----------------------------------------------------------------------
+/**
+	You could use the following T-SQL expression, which is based on
+	a correlated subquery, for this:
+**/
+SELECT orderid, orderdate, empid, custid,
+  (SELECT MAX(O2.orderid)
+   FROM Sales.Orders AS O2
+   WHERE O2.orderid < O1.orderid) AS prevorderid
+FROM Sales.Orders AS O1;
+
+/**
+	Similarly, you can phrase the concept of "next" as "the minimum value
+	that is greater than the current value." The following is the query
+	that returns for each order the next order ID:
+**/
+SELECT orderid, orderdate, empid, custid,
+  (SELECT MIN(O2.orderid)
+   FROM Sales.Orders AS O2
+   WHERE O2.orderid > O1.orderid) AS nextorderid
+FROM Sales.Orders AS O1;
+/**
+	Note: T-SQL supports window functions called LAG and LEAD that
+	you use to obtain elements from a previous or next row much more
+	easily.
+**/
+
+----------------------------------------------------------------------
+-- Using Running Aggregates
+----------------------------------------------------------------------
+-- Running aggregates are aggregates that accumulate values based on
+-- some order.
+----------------------------------------------------------------------
+SELECT orderyear, qty
+FROM Sales.OrderTotalsByYear;
+
+/**
+	Suppose you need to compute for each year the running total quantity
+	up to and including that year's. For the earliest year recorded in
+	the view (2020), the running total is equal to that year's quantity.
+	For the second year (2021), the running total is the sum of the first
+	year plus the second year, and so on.
+
+	You query one instance of the view(call it O1) to return for each year
+	the current year and quantity. You use a correlated subquery against a
+	second instance of the view(call it O2) to calculate the running-total
+	quantity. The subquery should filter all rows in O2 where the order year
+	is smaller than or equal to the current year O1, and sum the quantities
+	from O2.
+**/
+SELECT orderyear, qty,
+  (SELECT SUM(O2.qty)
+   FROM Sales.OrderTotalsByYear AS O2
+   WHERE O2.orderyear <= O1.orderyear) AS runqty
+FROM Sales.OrderTotalsByYear AS O1
+ORDER BY orderyear;
+
+/**
+	Note that T-SQL supports window aggregate functions, which you can use
+	to compute running totals much more easily and efficiently.
+**/
